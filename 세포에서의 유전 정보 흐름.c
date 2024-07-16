ce13bcfbc86614e29f85dc0a21209f4abcb4b39a
wjdtndk <166589877+wjdtndk@users.noreply.github.com>
2024-07-16 21:29:05 +0900

    Update 세포에서의 유전 정보 흐름.c

--- 세포에서의 유전 정보 흐름.c
+++ 세포에서의 유전 정보 흐름.c
@@ -5,6 +5,7 @@
 int main() {
     char DNA[500], RNA[500];
     int i;
+    int start = -1;
     char amy[3];
 
     printf("[ 세포에서의 유전 정보 흐름을 알아봅시당 >< ]\n");
@@ -24,15 +25,31 @@ int main() {
         else if ((DNA[i] == 'g') || (DNA[i] == 'G')) {
             RNA[i] = 'C';
         }
+        else {
+            printf("\n 염기가 아닌 문자가 입력되었습니다 : %c \n", DNA[i]);
+            return 1;
+        }
     }
     RNA[strlen(DNA)] = '\0';
 
     printf("\n전사 결과 : %s\n", RNA);
 
+    for (i = 0;i < strlen(RNA) - 2;i++) {
+        if ((RNA[i] == 'A') && (RNA[i + 1] == 'U') && (RNA[i + 2] == 'G')) {
+            start = i;
+            break;
+        }
+    }
+
+    if (start == -1) {
+        printf("\nAUG 코돈(개시 코돈)이 존재하지 않습니다 \n");
+        return 1;
+    }
+
     printf("\n번역 결과 : ");
 
-    for (int i = 0; i < strlen(RNA); i += 3) {
-        if (i + 2 < strlen(RNA)) { 
+    for (int i = start; i < strlen(RNA); i += 3) {
+        if (i + 2 < strlen(RNA)) {
             amy[0] = RNA[i];
             amy[1] = RNA[i + 1];
             amy[2] = RNA[i + 2];
@@ -47,7 +64,7 @@ int main() {
                 printf("아이소류신   ");
             }
             else if ((amy[0] == 'A') && (amy[1] == 'U') && (amy[2] == 'G')) {
-                printf("메싸이오닌(시작코돈)   ");
+                printf("메티오닌(개시 코돈)   ");
             }
             else if ((amy[0] == 'G') && (amy[1] == 'U') && (amy[2] == 'U') || (amy[0] == 'G') && (amy[1] == 'U') && (amy[2] == 'C') || (amy[0] == 'G') && (amy[1] == 'U') && (amy[2] == 'A') || (amy[0] == 'G') && (amy[1] == 'U') && (amy[2] == 'G')) {
                 printf("발린   ");
@@ -69,6 +86,7 @@ int main() {
             }
             else if ((amy[0] == 'U') && (amy[1] == 'A') && (amy[2] == 'A') || (amy[0] == 'U') && (amy[1] == 'A') && (amy[2] == 'G') || (amy[0] == 'U') && (amy[1] == 'G') && (amy[2] == 'A')) {
                 printf("종결 코돈   ");
+                return 1;
             }
             else if ((amy[0] == 'C') && (amy[1] == 'A') && (amy[2] == 'U') || (amy[0] == 'C') && (amy[1] == 'A') && (amy[2] == 'C')) {
                 printf("히스티딘   ");
